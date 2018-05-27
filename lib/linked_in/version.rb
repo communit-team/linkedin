6c4cccd798a6d89cdd47ed7328209f6ae444de1f
Ran Margaliot <ran@commun.it>
2018-05-27 12:59:57 +0300

    testing commenting of code

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 1 
-    PATCH = 7
+    PATCH = 8
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
