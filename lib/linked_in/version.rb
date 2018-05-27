af2337277388caca4dbd8dced1d6ada7b05a0b79
roeesheelo <roee@commun.it>
2018-05-27 10:49:35 +0300

    bumped v

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 1
     MINOR = 1 
-    PATCH = 6
+    PATCH = 7
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
